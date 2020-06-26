a6d44cf029578acabbb825d9fbfa94f93d2634c3
Matt <matt@pop-os.localdomain>
2020-06-26 09:54:18 -0400

    Updates

---
+++ precache-manifest.3edc9e9a8f1fdf874a5efb1e4d4af561.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54799b16a581a1a83c142c27912df88",
+    "url": "/cah/index.html"
+  },
+  {
+    "revision": "e23a4836f28b22eac68c",
+    "url": "/cah/static/css/2.0bc42666.chunk.css"
+  },
+  {
+    "revision": "6c395a33c9cd10d39ef3",
+    "url": "/cah/static/css/main.83b66c11.chunk.css"
+  },
+  {
+    "revision": "e23a4836f28b22eac68c",
+    "url": "/cah/static/js/2.7d501d4f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cah/static/js/2.7d501d4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c395a33c9cd10d39ef3",
+    "url": "/cah/static/js/main.6e85ba5f.chunk.js"
+  },
+  {
+    "revision": "5e5a1de3afb896e508c2",
+    "url": "/cah/static/js/runtime-main.4e85dd7f.js"
+  },
+  {
+    "revision": "5f7a1909cdc1c51f8c4bfc199da8d5d2",
+    "url": "/cah/static/media/cloud1.5f7a1909.svg"
+  },
+  {
+    "revision": "83a5d280d3725ddaec6a0fbf3bbff5d2",
+    "url": "/cah/static/media/heart-beat.83a5d280.svg"
+  }
+]);
